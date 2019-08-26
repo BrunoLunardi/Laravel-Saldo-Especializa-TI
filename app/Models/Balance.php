50ec64e646b016fa1960ccc2717b1be225d3c045
bruno <nardi273@gmail.com>
2019-08-26 17:20:55 -0300

    Implementação de Transaction no saldo de historico

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+//para utilizar método de banco de dados (transações, por exemplo)
+use DB;
+
 class Balance extends Model
 {
     //para desativar o timestamps desta tabela
@@ -11,6 +14,10 @@ class Balance extends Model
 
     //função para deposito (retorna um array)
     public function deposit(float $value) : Array{
+
+        //Inicializa transação do banco de dados (ou toda operação é executada ou nada será executado)
+        DB::beginTransaction();
+
         //saldo atual
         //$this é o objeto de balance vindo do controller
         //dd($this->amount);
@@ -39,17 +46,27 @@ class Balance extends Model
 
         //Verifica se inseriu com sucesso o deposito e o historico
         if($deposit && $historic){
+
+            //realiza commit dos dados
+            DB::commit();
+
+
             return [
                 'success' => true,
                 'message' => 'Sucesso ao recarregar'
             ];
-        }
+        }else{
 
-        //se não inseriu com sucesso
-        return [
-            'success' => false,
-            'message' => 'Falha ao recarregar'
-        ];
+            //se der erro ao inserir dados, realiza rollback
+            DB::rollback();
+
+            //se não inseriu com sucesso
+            return [
+                'success' => false,
+                'message' => 'Falha ao recarregar'
+            ];
+
+        }
 
     }
 
